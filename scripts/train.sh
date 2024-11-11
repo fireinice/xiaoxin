592402b8bbb9229fc7ee3de868f5325b97c5e7e6
yongbo.gyb <yongbo.gyb@alibaba-inc.com>
2024-11-11 15:45:47 +0800

    add

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export NCCL_DEBUG=INFO
-python train_DTI.py --exp-id bindingdb_v2 --config configs/default_config.yaml
+python train_DTI.py --exp-id bindingdb_v2_baseline --config configs/default_config.yaml
---
+++ scripts/train_bindingdb_v2_baseline.sh
@@ -0,0 +1,2 @@
+export NCCL_DEBUG=INFO
+python train_DTI.py --exp-id bindingdb_v2_baseline --config configs/bindingdb_v2_baseline.yaml
